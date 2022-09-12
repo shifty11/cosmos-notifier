ab1e02c42f2a38664eae0093ca0cab7ebed1729f
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-09-12 11:14:22 +0200

    remove webapp from production.yml

--- webapp/lib/env.dart
+++ webapp/lib/env.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/foundation.dart';
 
-const uri = kReleaseMode ? 'https://daodao-notifier.decrypto.online' : 'http://test.mydomain.com';
+const uri = kReleaseMode ? 'https://api.decrypto.online:8090' : 'http://test.mydomain.com';
